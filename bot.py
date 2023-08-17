cb4dd21f7b8cc140dc0f92cccc22c3e9f7570468
losyashfuse95 <losyashfuse95@gmail.com>
2023-08-17 08:19:35 +0300

    Complete

--- bot.py
+++ bot.py
@@ -1,17 +1,21 @@
 from aiogram import Bot, Dispatcher, executor, types
 import json
+import telebot
 
 API_TOKEN = '6104892713:AAHHeqhRVvHgpwFrMXq8CYBwH9q82L2B3KA'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-
+Telebot = telebot.TeleBot(API_TOKEN)
 
 def read_json(file_path):
     with open(file_path, 'r',encoding='utf-8') as file:
         data = json.load(file)
     return data
 
+def write_json(path, data):
+        with open(path, 'w') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
 @dp.message_handler()
 async def forward(message: types.Message):
@@ -26,8 +30,10 @@ async def forward(message: types.Message):
     if title in Data["topics"]:
         await bot.copy_message(chat_id=-1001926317748, message_thread_id=Data["topics"][title], protect_content=True, message_id=message.message_id, from_chat_id=-1001926317748)
     else:
-        await bot.send_message(-1001926317748, f"Укажите в файле json номер топика для заказчика: \n{title}!!!!!!")
-        return 0
+        temp = Telebot.create_forum_topic(chat_id=-1001926317748, name=title)
+        Data["topics"][title] = temp.message_thread_id
+        write_json("Bot/db.json",Data)
+        await bot.copy_message(chat_id=-1001926317748, message_thread_id=Data["topics"][title], protect_content=True, message_id=message.message_id, from_chat_id=-1001926317748)
     await message.delete()
 
 
